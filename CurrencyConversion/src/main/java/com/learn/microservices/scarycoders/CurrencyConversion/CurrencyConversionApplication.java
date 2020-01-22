d682c138532db623e34690f13e7ff09cc53d4712
ruksadsiddiqui <sidsid2810@gmail.com>
2020-01-22 17:14:46 +0530

    microservices setup ribbon feign edureka

--- CurrencyConversion/src/main/java/com/learn/microservices/scarycoders/CurrencyConversion/CurrencyConversionApplication.java
+++ CurrencyConversion/src/main/java/com/learn/microservices/scarycoders/CurrencyConversion/CurrencyConversionApplication.java
@@ -2,12 +2,14 @@ package com.learn.microservices.scarycoders.CurrencyConversion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class CurrencyConversionApplication {
 
 	public static void main(String[] args) {
--- ForexServiceApplication/src/main/java/com/learn/microservices/scarycoders/ForexServiceApplication/ForexServiceApplication.java
+++ ForexServiceApplication/src/main/java/com/learn/microservices/scarycoders/ForexServiceApplication/ForexServiceApplication.java
@@ -2,8 +2,10 @@ package com.learn.microservices.scarycoders.ForexServiceApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ForexServiceApplication {
 
 	public static void main(String[] args) {
