ff7856f8242ce78684e3e2a7e03211f354efcc3b
ruksadsiddiqui <sidsid2810@gmail.com>
2020-01-22 15:45:22 +0530

    microservices setup setting load balance

--- CurrencyConversion/src/main/java/com/learn/microservices/scarycoders/CurrencyConversion/proxy/CurrencyExchangeService.java
+++ CurrencyConversion/src/main/java/com/learn/microservices/scarycoders/CurrencyConversion/proxy/CurrencyExchangeService.java
@@ -1,11 +1,13 @@
 package com.learn.microservices.scarycoders.CurrencyConversion.proxy;
 
 import com.learn.microservices.scarycoders.CurrencyConversion.entity.CurrencyConversionBean;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="forex-service",url = "localhost:8082")
+@FeignClient(name="forex-service")
+@RibbonClient(name="forex-service")
 public interface CurrencyExchangeService {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
